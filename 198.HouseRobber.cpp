0ebbd9acc454cab5023934e18b2d024f1dbd0c0d
Wenbo Dai <daiwenbo@gmail.com>
2016-02-02 10:27:38 -0500

    Update 198.HouseRobber.cpp

--- 198.HouseRobber.cpp
+++ 198.HouseRobber.cpp
@@ -4,7 +4,7 @@ You are a professional robber planning to rob houses along a street. Each house 
 Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
 */
 
-/*Your runtime beats 6.16% of cpp submissions*/
+/*Your runtime beats 6.16% of cpp submissions*/ 
 
 class Solution {
 public:
